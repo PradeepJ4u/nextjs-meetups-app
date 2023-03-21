6b0cc9388824819e1109abfd0101deabcbf3ba57
Pradeep Joshi <pradeepjoshimca2006@gmail.com>
2023-03-21 10:14:21 +0530

    Image tag chnges

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import styles from './MeetupDetails.module.css'
 function MeetupDetails(props) {
   return (
     <section className={styles.details}>
-      <img src={props.img} alt={props.title} />
+      <Image src={props.img} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
