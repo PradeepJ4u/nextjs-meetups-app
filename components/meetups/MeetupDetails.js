801c12880e10dcbb4da4d19740ac1a7996e11aee
Pradeep Joshi <pradeepjoshimca2006@gmail.com>
2023-03-21 10:40:09 +0530

    image issue fixed

--- components/meetups/MeetupDetails.js
+++ components/meetups/MeetupDetails.js
@@ -1,9 +1,8 @@
-import Image from 'next/image';
 import styles from './MeetupDetails.module.css'
 function MeetupDetails(props) {
   return (
     <section className={styles.details}>
-      <Image src={props.img} alt={props.title} />
+      <img src={props.img} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
